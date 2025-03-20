932a965087d0461ca9f3f98da82a77b76b0d6544
Prithviraj Mazumder <prithirajmajumder8@gmail.com>
2025-03-21 01:43:17 +0530

    feat: puts old page.tsx in v1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,5 @@
-'use client'
-import gsap from 'gsap'
-import { Cursor } from '@/components/cursor'
-import { InitialLoader } from '@/components/initial-loader'
-import { NameIntro } from '@/components/name-intro'
-import ReactLenis, { LenisRef } from 'lenis/react'
-import { useEffect, useRef, useState } from 'react'
-
-export default function Home() {
-  const [loadComplete, setLoadComplete] = useState(false)
-  const lenisRef = useRef<LenisRef>(null)
-
-  useEffect(() => {
-    function update(time: number) {
-      lenisRef.current?.lenis?.raf(time * 1000)
-    }
-
-    gsap.ticker.add(update)
-
-    return () => gsap.ticker.remove(update)
-  }, [])
-
-  return (
-    <>
-      <Cursor isLoading={!loadComplete} />
-      <ReactLenis root options={{ autoRaf: true }} ref={lenisRef}>
-        {loadComplete && <NameIntro />}
-        {!loadComplete && <InitialLoader onLoadComplete={() => setLoadComplete(true)} />}
-      </ReactLenis>
-    </>
-  )
+const Home = () => {
+  return <div>Home</div>
 }
+
+export default Home
