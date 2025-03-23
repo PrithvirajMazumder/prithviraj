7e2be09399688b9dd5492f530f133d3af1bc97f4
Prithviraj Mazumder <prithirajmajumder8@gmail.com>
2025-03-24 00:31:36 +0530

    feat: adds smooth scrolling effect using lenis

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,27 @@
 'use client'
-
 import { LandingSection } from '@/components/sections/landing-section'
+import gsap from 'gsap'
+import ReactLenis, { LenisRef } from 'lenis/react'
+import { useEffect, useRef } from 'react'
 
-const Home = () => {
+export default function App() {
+  const lenisRef = useRef<LenisRef>(null)
+
+  useEffect(() => {
+    function update(time: number) {
+      lenisRef.current?.lenis?.raf(time * 1000)
+    }
+
+    gsap.ticker.add(update)
+
+    return () => gsap.ticker.remove(update)
+  }, [])
+
   return (
     <>
-      <LandingSection />
+      <ReactLenis root options={{ autoRaf: true }} ref={lenisRef}>
+        <LandingSection />
+      </ReactLenis>
     </>
   )
 }
-
-export default Home
